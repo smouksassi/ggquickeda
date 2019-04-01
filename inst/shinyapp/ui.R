54cb91415b394ad8f73a88c335bc9187ae0f9ae2
Samer Mouksassi <Samer.Mouksassi@certara.com>
2019-04-01 15:22:03 +0300

    fixed ui for km

--- inst/shinyapp/ui.R
+++ inst/shinyapp/ui.R
@@ -1253,17 +1253,14 @@ checkboxInput('rmyaxistickslabels', 'Remove Y axis ticks and labels ?',value=FAL
               ### KM section
               
               
-              tabPanel(
-                "Kaplan-Meier (CI)",
+              tabPanel("Kaplan-Meier (CI)",
                 fluidRow(
-                   column (
-                    3,
+                   column (3,
                     radioButtons("KM", "KM:",
                                  c("KM" = "KM",
                                    "KM/CI" = "KM/CI",
                                    "None" = "None") ,selected="None") ,
-                   conditionalPanel(
-                      " input.KM== 'KM/CI' ",
+                   conditionalPanel( " input.KM== 'KM/CI' ",
                       sliderInput("KMCI", "KM CI:", min=0, max=1, value=c(0.95),step=0.01),
                       sliderInput("KMCItransparency", "KM CI Transparency:", min=0, max=1, value=c(0.2),step=0.01)
                     ),
@@ -1274,79 +1271,64 @@ checkboxInput('rmyaxistickslabels', 'Remove Y axis ticks and labels ?',value=FAL
                                       selectizeInput("risktablevariables", 'Numbers to Show:',
                                                      choices = c("n.risk","n.event","n.censor","pct.risk","cum.n.event","cum.n.censor") ,multiple=TRUE,
                                                      selected = c("n.risk","n.censor")),
+                                      checkboxInput('addhorizontallines', 'Draw Horizontal lines',value = TRUE),
                                       sliderInput("nriskpositionscaler", "Numbers position scaler:", min=0.1, max=1, value=c(0.2),step=0.01),
-                                      sliderInput("nriskpositiondodge", "Numbers vertical dodge scaler:", min=-1, max=1, value=c(0.2),step=0.01),
-                                      checkboxInput('addhorizontallines', 'Draw Horizontal lines')
-                                      
-                    )
-                    )
-                  
-                    
-                  ),
-                  
+                                      sliderInput("nriskpositiondodge", "Numbers vertical dodge scaler:", min=-1, max=1, value=c(0.2),step=0.01)
+                    )#risktable
+                    )#kmnotnotnone
+                  ) ,#first km column
                   column (3,
                      conditionalPanel( " input.KM!= 'None' ",
                                       sliderInput("kmlinesize", "KM Line(s) Size:", min=0, max=6, value=c(1),step=0.1),
                                       sliderInput("kmlinealpha", "KM Line(s) Transparency:", min=0, max=1, value=c(0.5),step=0.01),
                                       radioButtons("addmediansurv", "Add Median Survival:",
-                                                   c("Median" ="addmediansurvival"  ,"Median/95%CI" = "addmediancisurvival","None" = "None")
-                                                   ,selected="None")
+                                                   c("Median" ="addmediansurvival"  ,
+                                                     "Median/95%CI" = "addmediancisurvival",
+                                                     "None" = "None")
+                                                    ,selected="None"),
+                                      conditionalPanel( " input.addmediansurv!= 'None' ",
+                                      checkboxInput('arrowmedian', 'Draw Arrow?')
+                                      )#addmedian
 
-                     ),
-                     conditionalPanel( " input.addmediansurv!=`None` ",
-                                       checkboxInput('arrowmedian', 'Draw Arrow?')
-                     )
-                     
-       ),
+                     )#kmnotnotnone
+        ),#second km column
                   column (3,
-                    conditionalPanel(
-                      " input.KM!= 'None' ",
+                    conditionalPanel(" input.KM!= 'None' ",
                       selectInput('KMtrans', label ='KM Transformation',
                                   choices=c("None" ="identity","event"="event",
                                             "cumhaz"="cumhaz","cloglog"="cloglog"),
                                   multiple=FALSE, selectize=TRUE,selected="identity"),
                       checkboxInput('censoringticks', 'Show Censoring Ticks?'),
                       checkboxInput('reversecenstoevent', 'Status is Censoring Flag ?') 
-                    )
-                  ),
+                    )#kmnotnotnone
+                  ),#third km column
                   column (3,
-                          conditionalPanel(
-                            " input.KM!= 'None' ",
+                          conditionalPanel(" input.KM!= 'None' ",
                             checkboxInput('kmignorecol', 'Ignore Mapped Color'),
-                            conditionalPanel(
-                              " input.kmignorecol ",
-                              conditionalPanel(
-                                " input.KM!= 'None' ",
+                            conditionalPanel(" input.kmignorecol ",
                                 colourpicker::colourInput(
                                   "colkml",
                                   "KM Line(s) Color",
-                                  value =
-                                    "black",
+                                  value = "black",
                                   showColour = "both",
-                                  allowTransparent =
-                                    FALSE,
+                                  allowTransparent = FALSE,
                                   returnName = TRUE
-                                )
-                              ),
+                                ),
                               conditionalPanel(
                                 " input.censoringticks ",
                                 colourpicker::colourInput(
                                   "colkmticks",
                                   "Censoring Tick(s) Color",
-                                  value =
-                                    "black",
+                                  value = "black",
                                   showColour = "both",
-                                  allowTransparent =
-                                    FALSE,
+                                  allowTransparent =FALSE,
                                   returnName = TRUE
                                 )
-                                
-                                
                               )#censorticks 
                             )#ignorecolor
                   )#kmnotnone
-                  ),#column
-                  column (12, h6("KM curves support is limited. When a KM curve is added nothing else will be plotted (e.g. points, lines etc.).Color/Fill/Group/Facets work." ))
+                  ),#column 4
+                  column (12, h6("KM curves support is now expanded. When a KM curve is added nothing else will be plotted (e.g. points, lines etc.).Color/Fill/Linetype/Group/Facets work." ))
                   
                   
                 )#fluidrow
