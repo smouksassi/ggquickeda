695412e2b65d5fb89efc8b352bd0005affaf02f0
Mouksassi <Samer.Mouksassi@ucsf.edu>
2026-02-01 12:19:10 -0800

    move examples to dont run to make cran happy

--- R/ggresponseexpdist.R
+++ R/ggresponseexpdist.R
@@ -139,6 +139,7 @@ plogis <- function(x) exp(x)/(1+exp(x))
 #'legend_order = c("color","shape","model"),
 #'color_legend_title ="Dose\nLevels")
 #'
+#'\dontrun{
 #'# Example 2
 #'ggresponseexpdist(data = effICGI |>
 #'dplyr::filter(Endpoint=="ICGI"),
@@ -176,8 +177,7 @@ plogis <- function(x) exp(x)/(1+exp(x))
 #'                  binlimits_color = "#475c6b",
 #'                  binlimits_ypos = 0.2,
 #'                  points_alpha= 0.1)
-#'                  
-#'\dontrun{
+#'   
 #'# Example 5 
 #' effICGI <- logistic_data |>
 #' dplyr::filter(!is.na(ICGI))|>
